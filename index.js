7aaa977ef3a46fd01e925ee3460646236d4616d3
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-06-02 18:16:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(drivers, name){
+  const nameLowerCase = name.toLowerCase();
+  return drivers.filter(driver => {
+    return driver.toLowerCase() == nameLowerCase;
+  });
+}
+
+function fuzzyMatch(drivers, leadingLetters){
+  const lengthToMatch = leadingLetters.length;
+  return drivers.filter(driver => {
+    return driver.slice(0, lengthToMatch) == leadingLetters;
+  });
+}
+
+function matchName(drivers, name){
+  //const matchingDrivers = [];
+  return drivers.filter(driver => {
+    return driver.name == name;
+  });
+}
